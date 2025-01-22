e6ab3ef77f22ab9f18c6f8980743091911edba49
Prasad Dananjaya Wilagama <85976286+PrasadWilagama@users.noreply.github.com>
2025-01-22 09:18:14 +0530

    Create R-Code-File.r

---
+++ R-Code-File.r
@@ -0,0 +1,42 @@
+# Basic R Commands
+
+# Arithmetic Operations
+a <- 10
+b <- 5
+sum <- a + b   # Addition
+diff <- a - b  # Subtraction
+prod <- a * b  # Multiplication
+quot <- a / b  # Division
+mod <- a %% b  # Modulus
+
+# Logical Operations
+x <- TRUE
+y <- FALSE
+and_op <- x & y  # AND operation
+or_op <- x | y   # OR operation
+not_x <- !x      # NOT operation
+
+# Vectors
+vec <- c(1, 2, 3, 4, 5)  # Create a vector
+vec_length <- length(vec) # Get vector length
+
+# Matrices
+mat <- matrix(1:9, nrow = 3, byrow = TRUE) # Create a 3x3 matrix
+mat_transpose <- t(mat)  # Transpose of a matrix
+
+# Data Frames
+data <- data.frame(Name = c("Alice", "Bob"), Age = c(25, 30))  # Create a data frame
+names <- data$Name  # Access a column
+
+# Functions
+square <- function(x) {
+  return(x^2)
+}
+squared_value <- square(4)  # Use the function
+
+# Reading and Writing Data
+write.csv(data, "data.csv")  # Write data to a CSV file
+read_data <- read.csv("data.csv")  # Read data from a CSV file
+
+# Plotting
+plot(vec, main = "Simple Plot", xlab = "Index", ylab = "Value", type = "o")  # Basic plot
